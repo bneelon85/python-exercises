4056374fa36db8fcd7384a8cdbc8f4337d5ccb23
Bobby Neelon <robert.neelon@gmail.com>
2018-03-09 21:41:55 +0000

    dict

---
+++ turtleGraphics.py
@@ -0,0 +1,41 @@
+# Exercise 1
+from turtle import *
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+forward(100)
+mainloop()
+
+
+# move into position
+up()
+forward(50)
+left(90)
+forward(50)
+left(90)
+down()
+# draw the square
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+left(90)
+forward(100)
+mainloop()
+
+
+pencolor('orange')
+width(10)
+circle(180)
+
+
+for i in range(5):
+    forward(100)
+    right(144)
+mainloop()
+
+
